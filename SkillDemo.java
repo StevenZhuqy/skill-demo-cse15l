6d1692f42eb767ba21403b5342c82c7c3edd3ab3
StevenZhuqy <102647267+StevenZhuqy@users.noreply.github.com>
2022-04-28 14:20:00 -0700

    change to code

--- SkillDemo.java
+++ SkillDemo.java
@@ -4,7 +4,7 @@ public class SkillDemo {
 
         int sum = 0;
         
-        for(int i = 0; i <= arr.length; i++){
+        for(int i = 0; i < arr.length; i++){
             sum += arr[i];
         }
 
