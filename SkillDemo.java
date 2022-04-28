b2600465b9eedaa125f0abf1dc90285fa1a01392
StevenZhuqy <102647267+StevenZhuqy@users.noreply.github.com>
2022-04-28 14:16:30 -0700

    JUnit Test Set up

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,13 @@
 public class SkillDemo {
 
+    public static int getSum(int[] arr){
+
+        int sum = 0;
+        
+        for(int i = 0; i <= arr.length; i++){
+            sum += arr[i];
+        }
+
+        return sum;
+    }
 }
